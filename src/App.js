7230de369b7090206de93cd1aeb8097fe048f590
DOCOPAC123 <rohitchira@gmail.com>
2021-03-26 10:12:07 +0530

    useContext hooks part done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import React from "react";
 import ClassCounter from "./components/useState/ClassCounter"
 import HooksClassCounter from "./components/useState/HooksClassCounter"
 import HooksClassCounterTwo from "./components/useState/HooksClassCounterTwo";
 import HooksClassCounterThree from "./components/useState/HooksClassCounterThree";
 
+export const UserContext = React.createContext();
+export const ProjectContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
-      <HooksClassCounter/>
-      <br/>
-      <p>Hooks Counter Example 2</p>
-      <HooksClassCounterTwo/>
-
-      <p>Hooks Counter Example 3</p>
-      <HooksClassCounterThree/>
+      <UserContext.Provider value="Rohit">
+        <ProjectContext.Provider value="Coding ki Marli">
+          <p>Hooks Counter Example 3</p>
+          <HooksClassCounterThree/>
+        </ProjectContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
--- src/components/useState/HooksClassCounterThree.js
+++ src/components/useState/HooksClassCounterThree.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import {UserContext, ProjectContext} from "../../App"
 
 function HooksClassCounterThree() {
     const[name, updateName] = useState({firstName : '', lastName : ''})
-
+    const user = useContext(UserContext);
+    const project = useContext(ProjectContext);
     return(
         <div>
             <input 
@@ -22,6 +24,8 @@ function HooksClassCounterThree() {
 
             <h2>Your First Name is - {name.firstName}</h2>
             <h2>Your Second Name is - {name.lastName}</h2>
+            <h2>User Name is - {user}</h2>
+            <h2>Project Name is - {project}</h2>
         </div>
     )
 } 
